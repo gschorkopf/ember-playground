852f6836cf089f783367b09e42dac266d2fa8d63
Geoff Schorkopf <gschorkopf@gmail.com>
2014-09-02 22:03:47 -0400

    All the right Ember pieces in place

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberCrm::Application.config.secret_key_base = 'a6bd17595693ce6fbd35a4760a3a4ad12047bb51ede83f49c857b4c38964bccf8c033c007418cdadd66f79a2c9d1292032f0add6a8759988858c6f532b66b2c5'
